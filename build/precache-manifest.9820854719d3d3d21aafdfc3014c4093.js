e64aa728caa6d867cc5ae566b9d6d54479503beb
Hasib Sarvari <tkdparallax@gmail.com>
2020-04-13 21:50:20 +0800

    #1

---
+++ build/precache-manifest.9820854719d3d3d21aafdfc3014c4093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed3aa27f04ae556534c4408ca5afd54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e357cf3417bc4d912a9",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "1538cf265ebce00dd566",
+    "url": "/static/js/2.2175e3b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2175e3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e357cf3417bc4d912a9",
+    "url": "/static/js/main.d73bc927.chunk.js"
+  },
+  {
+    "revision": "ca2ec7659abf2b3d42ac",
+    "url": "/static/js/runtime-main.b585dc48.js"
+  }
+]);
